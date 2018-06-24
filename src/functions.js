5bb4601afbc6f31ac3ca778083e303cfa29ddadb
Scott Allen <scott@jutsu.co>
2018-06-24 15:07:03 -0500

    Compile source to ES2015.

--- src/functions.js
+++ src/functions.js
@@ -1,37 +1,35 @@
-"use strict";
-
-var funclist = {};
-funclist["linear"] = function (x) {
+const funclist = {};
+funclist["linear"] = function(x) {
   return x;
 };
-funclist["quadratic"] = function (x) {
+funclist["quadratic"] = function(x) {
   return Math.pow(x, 2);
 };
-funclist["cubic"] = function (x) {
+funclist["cubic"] = function(x) {
   return Math.pow(x, 3);
 };
-funclist["quartic"] = function (x) {
+funclist["quartic"] = function(x) {
   return Math.pow(x, 4);
 };
-funclist["quintic"] = function (x) {
+funclist["quintic"] = function(x) {
   return Math.pow(x, 5);
 };
-funclist["sigmoid"] = function (x) {
-  return 1 / (1 + Math.exp(-x));
-};
-var sinusoidal = function sinusoidal(x) {
+funclist["sigmoid"] = function(x) {
+  return 1 / (1 + Math.exp(-x))
+}
+const sinusoidal = function(x) {
   return Math.sin(x * (Math.PI / 2));
 };
 funclist["sinusoidal"] = sinusoidal;
 funclist["sin"] = sinusoidal;
-var exponential = function exponential(x) {
+var exponential = function(x) {
   return Math.pow(2, 10 * (x - 1));
 };
 funclist["exponential"] = exponential;
 funclist["expo"] = exponential;
 funclist["exp"] = exponential;
-funclist["circular"] = function (x) {
-  return 1 - Math.sqrt(1 - x * x);
+funclist["circular"] = function(x) {
+  return 1 - Math.sqrt(1 - x*x)
 };
 funclist["uniqueList"] = ["linear", "quadratic", "cubic", "quartic", "quintic", "sinusoidal", "exponential", "circular", "sigmoid"];
 
